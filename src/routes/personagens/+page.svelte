1597ac1c6bc34a5d71c481a009d16e34719ba0c7
clarinharocha <clararocha.s30@gmail.com>
2025-01-23 12:20:11 -0400

    adicionei os personagens

--- src/routes/personagens/+page.svelte
+++ src/routes/personagens/+page.svelte
@@ -1,1 +1,14 @@
 <h1>Temporadas Ordem Paranormal</h1>
+
+<script>
+    import CartaMagic from '$lib/components/CartaMagic.svelte';
+    import { cards } from '$lib/data.js';
+  </script>
+   
+  <div class="row g-4">
+    {#each cards as card}
+      <div class="col">
+        <CartaMagic {...card} />
+      </div>
+    {/each}
+  </div>
