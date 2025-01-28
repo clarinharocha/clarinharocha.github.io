59ec26729ce82f621005e3f7305ac9098915e370
clarinharocha <clararocha.s30@gmail.com>
2025-01-28 10:55:25 -0400

    Imagens configuradas

--- src/lib/livroslancamentos.js
+++ src/lib/livroslancamentos.js
@@ -1,18 +1,62 @@
 export const livrosl = [
     {
-        titulo: 'Invisibility',
-        img: 'https://www.mtgpics.com/pics/art_th/alp/126.jpg'
+        titulo: 'A 5 passos de você',
+        img: 'a 5 passos de voce.jpg'
     },
     {
-        titulo: 'Invisibility2',
-        img: 'https://www.mtgpics.com/pics/art_th/alp/126.jpg'
+        titulo: 'A biblioteca da meia noite',
+        img: 'a biblioteca da meia noite.jpg'
+    },     
+    {
+        titulo: 'A garota do lago',
+        img: 'a garota do lago.jpg'
+    },     
+    {
+        titulo: 'A maldição do mar',
+        img: 'a maldição do mar.jpg'
+    },     
+    {
+        titulo: 'A vida invisível de Addie Larue',
+        img: 'a vida invisivel de addie larue.jpg'
+    },     
+    {
+        titulo: 'As mil partes do meu coração',
+        img: 'as mil partes do meu coração.jpg'
+    },
+    {
+        titulo: 'É assim que acaba',
+        img: 'é assim que acaba.jpg'
+    },
+    {
+        titulo: 'O lado feio do amor',
+        img: 'o lado feio do amor.jpg'
+    },
+    {
+        titulo: 'O príncipe cruel',
+        img: 'O principe cruel.jpg'
+    },
+    {
+        titulo: 'O verão que mudou minha vida',
+        img: 'o verao que mudou minha vida.jpg'
     },    
     {
-        titulo: 'Invisibility3',
-        img: 'https://www.mtgpics.com/pics/art_th/alp/126.jpg'
+        titulo: 'Os sete maridos de Evelyn Hugo',
+        img: 'os sete maridos de evlyn hugo.jpg'
     },    
     {
-        titulo: 'Invisibility4',
-        img: 'https://www.mtgpics.com/pics/art_th/alp/126.jpg'
+        titulo: 'Pedra filosofal',
+        img: 'pedra filosofal.jpg'
+    },    
+    {
+        titulo: 'Teto para dois',
+        img: 'teto para dois.jpg'
+    },    
+    {
+        titulo: 'Um caso perdido',
+        img: 'um caso perdido.jpg'
+    },    
+    {
+        titulo: 'Vermelho branco e sangue azul',
+        img: 'vermelho branco e sangue azul.jpg'
     },    
 ];
